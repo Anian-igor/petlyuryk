22bd71771ce7419ddf1630f69d2ce7f7b3309aca
Anian-igor <Ihor.holoveshko@gmail.com>
2024-01-02 23:26:24 +0200

    Revert "Update module-ru.ts"
    This reverts commit 7e47fd4274abe8ea4e1e88e057f7c9af37e384bc.

--- src/neural/modules/module-ru.ts
+++ src/neural/modules/module-ru.ts
@@ -8,7 +8,7 @@ import UaAggressiveResponseRussian from '~/data/responses/ua-hostile-russian.jso
 import UaAggressiveResponsePiss from '~/data/responses/ua-hostile-piss.json';
 import RuInsults from '~/data/insults/ru.json';
 import RuCommon from '~/data/common/ru.json';
-import RuHack from '~/data/common/ru-hack.json';
+
 
 export default new NeuralCorpus({
 	name: 'Russian Misc',
@@ -39,29 +39,3 @@ export default new NeuralCorpus({
 	],
 });
 
-export default new NeuralCorpus({
-	name: 'Russian Misc hack',
-	locale: 'uk',
-	data: [
-		{
-			intent: 'None',
-			utterances: [
-				...RuHack,
-			],
-			answers: [
-				...UaAggressiveResponseGeneric,
-				...UaAggressiveResponseRussian,
-				...UaAggressiveResponsePiss,
-				'Не пукай',
-				'Хрюкни ще раз, в тебе непогано виходить',
-				'Свинко, ти забула де твій хлів знаходиться?',
-				'А тепер напиши те ж саме, тільки нормальною мовою.',
-				'Погано розумію свинособачу, а ну-мо повтори ще разок.',
-				'Я не знаю російської. Може спробуєш державною?',
-				'Не розумію про що ти. Щось на роснявій...',
-				'Друзі, наша русофобія недостатня.',
-				'Як ти потішно хрюкаєш.',
-			],
-		},
-	],
-});
