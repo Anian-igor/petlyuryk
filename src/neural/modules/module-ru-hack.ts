e87356b80578d6774416b56009a8dd4c6300c163
Anian-igor <Ihor.holoveshko@gmail.com>
2024-01-02 23:18:57 +0200

    Create module-ru-hack.ts

---
+++ src/neural/modules/module-ru-hack.ts
@@ -0,0 +1,36 @@
+/**
+ * Part of Petlyuryk by SweetPalma, all rights reserved.
+ * This code is licensed under GNU GENERAL PUBLIC LICENSE, check LICENSE file for details.
+ */
+import { NeuralCorpus } from '..';
+import UaAggressiveResponseGeneric from '~/data/responses/ua-hostile-generic.json';
+import UaAggressiveResponseRussian from '~/data/responses/ua-hostile-russian.json';
+import UaAggressiveResponsePiss from '~/data/responses/ua-hostile-piss.json';
+import RuHack from '~/data/common/ru-hack.json';
+
+export default new NeuralCorpus({
+	name: 'Russian Misc hack',
+	locale: 'uk',
+	data: [
+		{
+			intent: 'None',
+			utterances: [
+				...RuHack,
+			],
+			answers: [
+				...UaAggressiveResponseGeneric,
+				...UaAggressiveResponseRussian,
+				...UaAggressiveResponsePiss,
+				'Не пукай',
+				'Хрюкни ще раз, в тебе непогано виходить',
+				'Свинко, ти забула де твій хлів знаходиться?',
+				'А тепер напиши те ж саме, тільки нормальною мовою.',
+				'Погано розумію свинособачу, а ну-мо повтори ще разок.',
+				'Я не знаю російської. Може спробуєш державною?',
+				'Не розумію про що ти. Щось на роснявій...',
+				'Друзі, наша русофобія недостатня.',
+				'Як ти потішно хрюкаєш.',
+			],
+		},
+	],
+});
